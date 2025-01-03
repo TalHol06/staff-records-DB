209de9f4077b5c0ebaedd95c718992454f4b915b
Ta'Lee Hollins-Bailey <taleeh98454@gmail.com>
2025-01-03 16:38:32 -0600

    added a selectTable function

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,50 @@
 import inquirer from 'inquirer';
+import pg from 'pg';
+
+const { Client } = pg;
+const db = new Client({
+  host: 'localhost',
+  user: 'postgres',
+  password: 'password1234',
+  database: 'staffrecords',
+  port: 5432
+});
+
+db.connect((err) => {
+  if (err){
+    console.log("Error connecting to the database: " +err.message);
+    return;
+  }
+  console.log("Connected to your database.")
+  fetchTable();
+});
+
+// Fetches all available tables from the database
+async function fetchTable(){
+  try{
+    const res = await db.query(
+      "SELECT table_name FROM information_schema.tables WHERE table_schema = 'pubic'"
+    );
+    console.log("Available tables:");
+    console.table(res.rows);
+    promptUser();
+  } catch (err){
+    console.log("Error fetching tables: " +err.message);
+  }
+}
+
+// Displays the choosen table to the user
+async function selectTable(tableName){
+  try{
+  const query = `SELECT * FROM ${tableName}`;
+  const res = await db.query(query);
+  console.log(`\nData from table "${tableName}"`);
+  console.table(res.rows);
+  } catch (err){
+    console.log(`Error selecting table ${tableName}: ` +err.message);
+  }
+  promptUser();
+}
 
 async function promptUser(){
   const answers = await inquirer.prompt([
@@ -24,10 +70,13 @@ async function promptUser(){
 
   if (answers.actions === "View all departments"){
     console.log(`Loading the "Department" table...`);
+    selectTable("Department");
   } else if(answers.actions === "View all roles"){
     console.log(`Loading the "Role" table...`);
+    selectTable("Role");
   } else if(answers.actions === "View all employees"){
     console.log(`Loading the "Employee" table...`);
+    selectTable("Employee");
   } else if(answers.actions === "Add a department"){
     console.log(`Adding your department...`);
   } else if(answers.actions === "Add a role"){
@@ -46,5 +95,3 @@ async function promptUser(){
     console.log(`Exiting prompt...`);
   }
 }
-
-promptUser();
